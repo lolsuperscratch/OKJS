5f5b5f9f4dc643c83f013ef804b986a363a4c92e
lolsuperscratch <lolsuperscratch@hotmail.com>
2018-08-07 04:32:01 +0000

    Create new file

---
+++ okjs.js
@@ -0,0 +1,27 @@
+var okjs = {
+
+   require = function (url) {
+         
+         var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       var functionreturn = new Function(xhttp.responseText);
+       functionreturn.call();
+       return functionreturn
+    }
+    if (!this.readyState == 4 && this.status == 200) {
+       console.error("error occured")
+       return;
+    }
+};
+xhttp.open("GET", file, true);
+xhttp.send();
+   },
+   foreach = function (array,fn) {
+      for (var thisone in array) {
+          fn.call(thisone)
+      }
+   }
+}
+
+window.OKJS = okjs;
